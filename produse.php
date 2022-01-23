445df9d8e8ca0471c1a4b635862946b0431237f0
Pyu19 <pyu.alex@yahoo.com>
2022-01-23 12:44:02 +0200

    Initial commit

---
+++ produse.php
@@ -0,0 +1,28 @@
+<?php
+
+$products = [
+    [
+    'name'      =>'Mere',
+    'pret'      =>3.19,
+    'rating'    =>3.75,
+    ],
+
+    [
+        'name'      =>'Pere',
+        'pret'      =>5.19,
+        'rating'    =>4.5,
+    ],
+
+    [
+        'name'      =>'Cirese',
+        'pret'      =>11.19,
+        'rating'    =>4.9,
+    ],  
+];
+
+for($i = 0; $i < count($products); $i++)
+{
+    echo "<b style="font-size: 20">"
+    echo
+    echo
+}
